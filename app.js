8a78f72b8839c9b0c510f3425383cc5f3faf7a3a
leul <leulzewduchanie@gmail.com>
2023-10-26 21:01:35 +0300

    updated the todolist

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 function add(){
+    let counter = 0;
+   
     let div = document.createElement('div')
-div.innerHTML = "<input type='text'  placeholder='write your text here' class='inpu1'>"
-    let place = document.querySelector(".place")
+div.innerHTML = " <input type='checkbox' class='check'><input type='text'  placeholder='write your text here' class='inpu1'> "
+  let place = document.querySelector(".place")
  place.append(div) 
+ 
 
+    
+    
 }
+
