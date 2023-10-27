03e26f384b2f7e7d0321442c982c4fc9214ca69f
leul <leulzewduchanie@gmail.com>
2023-10-27 19:47:02 +0300

    added delete button

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ function add(){
     let counter = 0;
    
     let div = document.createElement('div')
-div.innerHTML = " <input type='checkbox' class='check'><input type='text'  placeholder='write your text here' class='inpu1'> "
+div.innerHTML = " <input type='checkbox' class='check'><input type='text'  placeholder='write your text here' class='inpu1'> <input value='Delete' type='button' class='btn2' onclick='remove()'>"
   let place = document.querySelector(".place")
  place.append(div) 
  
 
     
     
+}
+function remove(){
+  
+  let del = document.querySelector('input')
+   del.remove()
+
 }
 
